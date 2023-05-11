c78b4e04e73970a3e9ca483489bb27ab483660e9
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-11 11:55:39 +0530

    Added: Delete Element

---
+++ Generic/Generic/DeleteArray.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class DeleteArray
+    {
+        public void DeleteInt(int[] arr, int index)
+        {
+            if (index < 0 || index >= arr.Length)
+            {
+                Console.WriteLine("Invalid index");
+                return;
+            }
+            int[] new_arr = new int[arr.Length - 1];
+            int new_index = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i != index)
+                {
+                    new_arr[new_index] = arr[i];
+                    new_index++;
+                }
+            }
+            Console.WriteLine($"Element at index {index} deleted successfully.");
+            for (int i = 0; i < new_arr.Length; i++)
+            {
+                Console.Write($"{new_arr[i]} ");
+            }
+        }
+
+        public void DeleteDouble(double[] arr, int index)
+        {
+            if (index < 0 || index >= arr.Length)
+            {
+                Console.WriteLine("Invalid index");
+                return;
+            }
+            double[] new_arr = new double[arr.Length - 1];
+            int new_index = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i != index)
+                {
+                    new_arr[new_index] = arr[i];
+                    new_index++;
+                }
+            }
+            Console.WriteLine($"\nElement at index {index} deleted successfully.");
+            for (int i = 0; i < new_arr.Length; i++)
+            {
+                Console.Write($"{new_arr[i]} ");
+            }
+        }
+
+        public void DeleteChar(char[] arr, int index)
+        {
+            if (index < 0 || index >= arr.Length)
+            {
+                Console.WriteLine("Invalid index");
+                return;
+            }
+            char[] new_arr = new char[arr.Length - 1];
+            int new_index = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i != index)
+                {
+                    new_arr[new_index] = arr[i];
+                    new_index++;
+                }
+            }
+            Console.WriteLine($"\nElement at index {index} deleted successfully.");
+            for (int i = 0; i < new_arr.Length; i++)
+            {
+                Console.Write($"{new_arr[i]} ");
+            }
+        }
+
+    }
+}
---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DeleteArray deleteArray = new DeleteArray();
+            // Delete an element from an array of integers
+            int[] int_arr = { 1, 2, 3, 4, 5 };
+            deleteArray.DeleteInt(int_arr, 2);
+
+            // Delete an element from an array of doubles
+            double[] double_arr = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            deleteArray.DeleteDouble(double_arr, 3);
+
+            // Delete an element from an array of characters
+            char[] char_arr = { 'a', 'b', 'c', 'd', 'e' };
+            deleteArray.DeleteChar(char_arr, 5);
+
+            Console.ReadKey();
+        }
+    }
+}
